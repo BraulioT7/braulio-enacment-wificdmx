c6588a4595c3cd0cd752324786666f0f83a9076a
Braulio Tellez <braulio.tellez.alvarado@gmail.com>
2026-03-05 23:04:17 -0600

    chore: setup firebase initialization and secure environment variables

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { initializeApp } from 'firebase/app';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {
+        initializeApp(environment.firebase);
+      },
+      multi: true
+    }
+  ]
 };
---
+++ src/environments/environment.sample.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    apiUrl: 'apiURL',
+    firebase: {
+    apiKey: "APIKEY",
+    authDomain: "AUTHDOMAIN",
+    projectId: "PROJECTID",
+    storageBucket: "STORAGEBUCKET",
+    messagingSenderId: "MESSAGINGSENDERID",
+    appId: "APPID",
+    measurementId: "MEASUREMENTID"
+  }
+};
