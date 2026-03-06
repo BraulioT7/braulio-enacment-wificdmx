beddedae260c719cc96e29ca99e80200bd948c92
Braulio Tellez <braulio.tellez.alvarado@gmail.com>
2026-03-05 20:18:34 -0600

    feat: install leaflet, setup lazy loaded map route and component skeleton

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'mapa'
+    },
+    {
+        path: 'mapa',
+        // Lazy Loading a nivel de componente
+        loadComponent: () => import('./features/map/components/map-shell/map-shell.component').then(m => m.MapShellComponent)
+    }
+];
---
+++ src/app/features/map/components/map-shell/map-shell.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-shell',
+  imports: [],
+  templateUrl: './map-shell.component.html',
+  styleUrl: './map-shell.component.scss'
+})
+export class MapShellComponent {
+
+}
